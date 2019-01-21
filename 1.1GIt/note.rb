6f7d2ef23636fa922c65c813cff6e6df7ab0edb7
Khoattd <Khoattd@nustechnology.com>
2019-01-21 15:23:13 +0700

    addFolders

--- 1.1GIt/note.rb
+++ 1.1GIt/note.rb
@@ -1,1 +1,8 @@
-https://quiz.nustechnology.com
+1) Có Git URL (ssh)
+2) config. (set url) git remote set-url origin <url>
+3) clone
+----------------------------------
+ssh-keygen -t rsa -C "Khoattd@nustechnology.com"
+
+Merge vs rebase 
+nên sử dụng merge khi trong project chung
