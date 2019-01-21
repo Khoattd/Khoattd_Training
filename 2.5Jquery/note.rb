12f86314021f32b0ac4c6a5f9f0ae20ba4cb16b1
Khoattd <Khoattd@nustechnology.com>
2019-01-21 16:43:26 +0700

    addNoteJquery

--- 2.5Jquery/note.rb
+++ 2.5Jquery/note.rb
@@ -1,1 +1,10 @@
-https://quiz.nustechnology.com
+Được xây dựng trên JS
+Jquery làm được thì JS làm được nhưng dài hơn (không có chiều ngược lại)
+mục tiêu: làm gọn gàng code
+Ajax: read2know
+Jquery Validation: plugin của jquery giúp kiểm tra độ valid của dữ liệu phía client,
+giảm tải cho server. (vd: khi client nhập sai yêu cầu về tên, gửi lên server, không 
+có jquevy validation thì server sẽ phải trả về, tốn tài nguyên). Jquery validation 
+sẽ check legit r ms gửi
+
+PHÂN BIỆT EACH VS MAP 
