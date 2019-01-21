b2e908a5a9b22abeff614671a79d7bfcba0148c5
GiauNV <giaunv@nustechnology.com>
2019-01-21 14:25:42 +0700

    up

--- note.rb
+++ note.rb
@@ -1,8 +1,1 @@
-nam cuoi, moi nhan de tai luan van
-trong vong 2 thang nua, 1-2 hours moi ngay
-suc khoe tot, ko di hoc them
-react, nodejs, heroku, javascript, C (viet nhieu nhat)
-SQL?
-MVC?
-OOP?
-len cong ty 4 ngay 1 tuan, tru thu 3
+https://quiz.nustechnology.com
