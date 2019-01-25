07ec96e726da2898f954daa1c9de28c7838051da
Khoattd <Khoattd@nustechnology.com>
2019-01-25 10:01:54 +0700

    firstRuby

---
+++ 3.Toolbox/note.rb
@@ -0,0 +1,7 @@
+RUBY LÀ GÌ
+là ngon ngữ lập trình backend được viết trên C++
+RUBY GEM là lib được viết trên ruBy để tối ưu hóa lập trình, làm nhanh hơn.
+một project gồm nhiều gem ghép lại  
+RVM : Ruby version manager: kiểm soát version của ruby,
+xuyên suốt project chỉ sử dụng một version ruby duy nhất, sử dụng RVM để cd qua các project
+khác nhau sẽ tự động chuyển đổi version của ruby 
