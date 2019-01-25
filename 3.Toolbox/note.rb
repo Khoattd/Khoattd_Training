12250703447afc524472bc053d7833fb0c276138
Khoattd <Khoattd@nustechnology.com>
2019-01-25 10:47:29 +0700

    updateRuby

--- 3.Toolbox/note.rb
+++ 3.Toolbox/note.rb
@@ -4,4 +4,21 @@ RUBY GEM là lib được viết trên ruBy để tối ưu hóa lập trình, l
 một project gồm nhiều gem ghép lại  
 RVM : Ruby version manager: kiểm soát version của ruby,
 xuyên suốt project chỉ sử dụng một version ruby duy nhất, sử dụng RVM để cd qua các project
-khác nhau sẽ tự động chuyển đổi version của ruby 
+khác nhau sẽ tự động chuyển đổi version của ruby \
+
+
+Quy tắt:
+Bắt đầu bằng chữ in: constant
+bắt đầu bằng $: global variable 
+bắt đầu bằng @: instance vabiable 
+             @@: class variable
+Method name có thể bắt đầu bằng chữ in 
+Ngoại trừ false và nil thì tất cả đều là true kể cả 0
+
+Check gem:
+gem search -r 
+            -r rails (global)
+            -l rails (local) or gem list
+INSTALL GEM :
+gem install [gem] 
+gem install [gem] [--version, -v] 
