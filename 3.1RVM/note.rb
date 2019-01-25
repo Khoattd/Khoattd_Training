b674d20aec30a6d837550ea10d8379852d864cb3
Khoattd <Khoattd@nustechnology.com>
2019-01-25 16:58:54 +0700

    endweek2

---
+++ 3.1RVM/note.rb
@@ -0,0 +1,10 @@
+command cơ bản:
+rvm --ruby-version use [version]: tạo file .ruby-version cho project
+rvm --ruby-version use [version]@gemname: tạo đồng thời cả .ruby-version và  ruby-gemset 
+rvm gemset create [name] tạo gemset cho ruby version hiện tại
+rvm gemset use/delete [name]
+rvm gemset list : list gemset 
+rvm list : list ruby 
+rvm use [version]: chuyển version ruby 
+ruby -v: check version ruby 
+rvm install [version]
