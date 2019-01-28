adf6c06ba642046f9a08b44f341de7b22d7f2149
Khoattd <Khoattd@nustechnology.com>
2019-01-28 08:31:29 +0700

    firstRuby Note

---
+++ 4.2 rubyBasic/note.rb
@@ -0,0 +1,13 @@
+open source - server side scripting language
+có thể nhúng vào HTMl
+scalable, big programs easily maintainable 
+có thể phát triển app internet, intranet 
+GUItool Suport : Tcl/tk, GTK, opeGL
+Nhiều function built sẵn 
+CASE SENSITIVE
+a + b = a+b (a là local variable)
+a +b = a(+b) (a là method)
+
+BEGIN {
+   code
+} gọi code trước khi programs chạy 
