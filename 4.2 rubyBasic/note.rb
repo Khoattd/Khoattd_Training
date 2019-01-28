775e79d9d57c890f2255861442728def9de3a7bd
Khoattd <Khoattd@nustechnology.com>
2019-01-28 08:59:42 +0700

    done Ruby Classes and Objects

--- 4.2 rubyBasic/note.rb
+++ 4.2 rubyBasic/note.rb
@@ -7,7 +7,36 @@ Nhiều function built sẵn
 CASE SENSITIVE
 a + b = a+b (a là local variable)
 a +b = a(+b) (a là method)
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+SYNTAX :
+print <<EOF
+   This is the first way of creating
+   here document ie. multiple line string.
+EOF
+
+print <<"EOF";                # same as above
+   This is the second way of creating
+   here document ie. multiple line string.
+EOF
+
+print <<`EOC`                 # execute commands
+	echo hi there
+	echo lo there
+EOC
+
+print <<"foo", <<"bar"  # you can stack them
+	I said foo.
+foo
+	I said bar.
+bar
 
 BEGIN {
    code
-} gọi code trước khi programs chạy 
+} gọi code trước khi programs chạy. ngược với END
+
+COMMENT :
+# THIS IS A COMMENT
+= begin 
+    multiple line comment 
+= end
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
