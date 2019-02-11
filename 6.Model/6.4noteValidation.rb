4eca785ae2d7c42650148f2a68741678e7e30d21
Khoattd <Khoattd@nustechnology.com>
2019-02-11 12:00:14 +0700

    update Validation

--- 6.Model/6.4noteValidation.rb
+++ 6.Model/6.4noteValidation.rb
@@ -1,7 +1,8 @@
 OVERVIEW 
     server-side validation  (ngoài ra còn client-side validation, controller level validation)
     WHY? 
-        ensure that only valid dâta go to the database 
+        ensure that only valid dâta go to the database (model-lever)
+        database anogstic, end user không thể qua mặt 
     WHEN? 
         khi chạy method như create,save. sẽ send SQL Command tới database. Validation sẽ chạy 
         ngay trước khi các command đó được thực hiện.
@@ -22,6 +23,73 @@ OVERVIEW
         + update_all, _attribut, _column, _columns, _counters
         + save(validate: false)
 VALID? INVALID? 
+    thực hiện validation mà khoogn cần các method trigger validation 
+    .errors.messages method để xem các errors
     vd:     Person.create(name: "John Doe").valid? # => true
             Person.create(name: nil).valid? # => false
-              
+ERRORS[:attribute]
+ method check xem atttribute có bị error không. true nếu error
+ CHỈ CHẠY SAU KHI ĐÃ THỰC HIỆN VALIDATION, VÌ NÓ CHỈ INSPECT ERROR COLLECTION 
+ CHỨ KHÔNG TRIGGER VALIDATIONS 
+            VD: Person.create.errors[:name].any? # => true
+
+ERRORS.DETAILS[:attriute]
+    check validation nào fail 
+            VD: a = Article.create(tittle: "khoa", text: "a")
+              a.errors.details[:tittle] =>>> trả về hash chứa tittle too short 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+VALIDATION HELPER 
+    sử dụng trực tiếp trong model class def 
+    tất cả các helper đều accept :on và :message option
+-------------------------------------------------------------------
+Acceptance 
+    kiếm tra checkbox đã check chưa. 
+    vd : validates :terms_of_service, acceptance: { message: 'must be abided' }
+    acceptance không cần phải record trong database, nếu không có trong database helper
+    sẽ tự tạo virtual attribute 
+
+Validate_associated
+ check valid với được liên kết với model này.
+ không sử dụng ở cả 2 cái được associated với nhau vì sẽ tạo loop  
+            vd: has_many :books
+                validates_associated :books
+Confirmation 
+ sử dụng để 2 text_field phải có 2 content giống nhau (password, email)
+ chỉ check khi field có đuôi _confirmation có content  
+    vd:   validates :email, confirmation: true
+          validates :email_confirmation, presence: true
+Exclusion/Inclusion
+    sử dụng để check attribute value không/phải được có trong given set 
+    vd: exclusion: { in: %w(www us ca jp),message: "%{value} is reserved." }
+            nhập www, us, ca, jp sẽ báo lỗi. nếu trong string có nó thì vẫn được vd wwww
+Format 
+    check attribute value có match với regular expression không 
+    vd :format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
+Length 
+    check độ dài của attribute values :
+    bao gồm: minimum, maximum, in, is 
+    option : too_long, wrong_length, too_short 
+            vd: length: { maximum: 1000,too_long: "%{count} is the maximum allowed" }
+Numericality
+    chỉ cho phép là số 
+    option: only_integer, even, odd : boolean 
+            greater_than, greater_than_or_equal_to
+            equal_to, less_than, less_than_or_equal_to
+            other_than,
+            vd: validates :points, numericality: true
+Presence/Absence 
+ check attribute value phải có/không có 
+Uniqueness 
+ check attribute value phải là duy nhất trước khi lưu vào database
+ thêm option scope, case_sensitive 
+Vd: validates :name, uniqueness: { scope: :year,message: "should happen once per year" }
+Validates_with 
+    check với class được code để validation, không có default error 
+Validates_each 
+    check các attribute với một block, block được pass record, attribute name, attribute value 
+    phải add error message vào model để nó invalid 
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+COMMON VALIDATION OPTION 
+:allow_nil bỏ qua validation nếu value được validate là nil 
+:allow_blank bỏ qua validation nếu value là blank 
+:message specify message được đưua vào error collection khi validation fails 
