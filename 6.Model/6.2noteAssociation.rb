3880527563e60e7966897a4e5aa5ccda099ab0db
Khoattd <Khoattd@nustechnology.com>
2019-02-13 16:08:28 +0700

    update Association

--- 6.Model/6.2noteAssociation.rb
+++ 6.Model/6.2noteAssociation.rb
@@ -125,6 +125,8 @@ BI-DIRECTIONAL APPLICATION
 ASSOCIATION REFERENCE
 -----------------------------------------------------------------   
 BELONGS_TO REFERENCE 
+    set 1 object trong belongs_to association sẽ không auto save object và associated object 
+
 METHOD ADDED 
     association
     association=(associate)
@@ -158,4 +160,131 @@ Create_association
 Create_association! raise ActiveRecord::RecordInvalid nếu record không pas validation 
 --------------  
 OPTIONS FOR BELONGS_TO 
+    sử dụng để customize behavior của belongs_to bằng cách pass options và scope block 
+        khi khởi tạo association 
+        vd:
+            class Book < ApplicationRecord
+            belongs_to :author, dependent: :destroy,
+                counter_cache: true
+            end
+    CÁC OPTIONS BAO GỒM :
+    :autosave boolean
+        Rails tự động save any loaded association members và destroy marked for destruction 
+        members khi parent object được save 
+        CHÚ Ý: autosave: false khác không set autosave 
+        nếu không set autosave, new associated object sẽ được save, updated assiated object 
+        sẽ không save   
+    :class_name
+        nếu tên của model kia không thể "derived" từ association name. sử dụng class_name 
+        để supply model name 
+            vd: sách thuộc về tác giả nhưng model chứa tác giả là Patron 
+            class Book < ApplicationRecord
+                belongs_to :author, class_name: "Patron"
+            end
+    :counter_cache
+        tìm belonging object nhanh hơn 
+        nếu không có option counter_cache, @author.books.size sẽ thực hiện COUNT(*) query 
+        thêm option counter_cache ở chỗ khai báo belongs_to (book) sẽ tọa cache value 
+        để lưu giá trị counter
+        TUY NHIÊN PHAỈ THÊM COLUMN _count (books_count) VÀO TABLE CỦA HAS_MANY MODEL 
+            vd: belongs_to :author, counter_cache: true 
+                                    counter_cache: :count_of_books #set tên cho column 
+        NHỮNG OBJECT ĐƯỢC TẠO TRƯỚC KHI TẠO COLUMN _count sẽ khuông được đếm 
+    :dependent
+        có 2 option 
+            :destroy khi object này bị destroy thì sẽ destroy luôn associated object 
+            :delete khi object này bị destroy thì associated object sẽ bị detele trực tiếp 
+                trong database mà không gọi lẹnh destroy 
+    :foreign_key
+        set name cho foreign_key. default là thêm suffix _id
+            vd: foreign_key: "khoa"
+        IN ANY CASE, RAIL SẼ KHÔNG TỰ TẠO COLUMN FOREIGN_KEY, PHẢI TỰ TẠO 
+        BẰNG MIGRATION 
+    :primary_key
+        chọn column khác column id làm primary key 
+        vd: 
+            class User < ApplicationRecord
+                self.primary_key = 'guid'  #set primary key của table User là guid
+            end
+            
+            class Todo < ApplicationRecord
+                belongs_to :user, primary_key: 'guid' 
+            end
+          #foreign_key của Todo table là user_id chứa data từ column guid của User 
+    :inverse_of
+    :polymorphic
+        pass true để nó là polymorphic association 
+    :touch
+        updated_at, updated_on timestamp sẽ được update time mỗi khi object save 
+        hoặc destroy
+    :validate
+        nếu set true, associated object sẽ được validate mỗi khi save object này 
+         vd: nếu trong books có belongs_to :author, validate: true 
+            mỗi khi save books thì author tương ứng sẽ được validate 
+    :optional
+        Nếu set true, associated object sẽ không validate presence 
+-----------------------------
+SCOPES FOR BELONGS_TO 
+    sử dụng khi cần customize belongs_to thông qua một scope block 
+    vd:             
+        class Book < ApplicationRecord
+            belongs_to :author, -> { where active: true },
+                                dependent: :destroy
+        end
+    CÓ THỂ SỬ DỤNG BẤT CỨ QUERYING METHOD NÀO 
+    --------------------------
+    Where
+        specify condition mà associated object phải có 
+        vd: -> { where active: true } ( thằng author phải có attribute active là true)
+    Includes 
+        specify second-order association - cái sẽ được load khi association này được sử dụng 
+        vd:  trogn trường hợp sử dụng nhiều @line_item.book.author
+            class LineItem < ApplicationRecord
+                belongs_to :book, -> { includes :author }
+            end
+            class Book < ApplicationRecord
+                belongs_to :author
+                has_many :line_items
+            end
+            class Author < ApplicationRecord
+                has_many :books
+            end
+    Readonly 
+        associated object chỉ có thể readonly khi retrived thoogn qua association 
+    Select 
+        override SQL SELECT (được sử dụng để retrive data của associated object). Default rails lấy all column 
+        nếu sử dụng method select trên belongs_to, cần phải set foregin_key option để kết quả đúng 
+---------------------------
+CHECK DOES ASSOCIATED OBJECT EXIST 
+    vd: if @book.author.nil?
+             @msg = "No author found for this book"
+        end
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+HAS_ONE ASSOCIATION REFERENCE 
+-----------------------------
+METHOD ADDED 
+    association
+    association=(associate)
+    build_association(attributes = {})
+    create_association(attributes = {})
+    create_association!(attributes = {})
+    reload_association
+    #association được thay bằng tên model được pass vào has_one 
+    #tương tự như belongs_to 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+!!!!!!!!!!!!!!!!! khi initialze belongs_to hoặc has_one. nên sử dụng build_prefix để build association thay vì 
+sử dụng .build method (sử dụng cho has_many, has_and_belongs_to_many). tương tự với create
